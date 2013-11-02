d3db5f4c2b68358e751f955feab289e484b3da84
tatums <tatum@ashlandstudios.com>
2013-11-02 12:44:03 -0500

    more vim stuff

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc"    # list of files/folders to symlink in homedir
+files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
