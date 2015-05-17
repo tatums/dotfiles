9d399527906eecc9f0c759a3211f940c8b33d851
Tatum <tatum@ashlandstudios.com>
2015-05-17 15:09:04 -0500

    update formatting && README

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 dotfiles
 ========
-![gun](http://i.ytimg.com/vi/YoU2hlDJmFE/maxresdefault.jpg)
 
 These are my dotfiles - There are many like them... But these are mine.
+
+
+![gun](http://i.ytimg.com/vi/YoU2hlDJmFE/maxresdefault.jpg)
+
 
 Run the command below to install them.
 
@@ -10,6 +13,3 @@ Run the command below to install them.
   bash setup.sh
 ```
 
-## NOTE
-
-open vim and :BundleInstall
--- setup.sh
+++ setup.sh
@@ -13,23 +13,23 @@ files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc vim gitconfig gi
 ##########
 
 # create dotfiles_old in homedir
-echo "\n\n-Creating $olddir for backup of any existing dotfiles in ~"
+echo "--Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 
 # change to the dotfiles directory
-echo "\n\n-Changing to the $dir directory"
+echo "--Changing to the $dir directory"
 cd $dir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "\n--Moving any existing dotfiles from ~ to $olddir"
+    echo "----Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "\n--Creating symlink to $file in home directory."
+    echo "----Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
-echo "\n\n-Setting up Vundle"
+echo "--Setting up Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "\n\n-Installing Plugins"
+echo "--Installing Plugins"
 vim +PluginInstall +qall
