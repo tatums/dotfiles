23fdc756345434e845d3c35f4ad148abe80310ca
Tatum <tatum@ashlandstudios.com>
2015-06-04 18:15:53 -0500

    add screenrc file

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc vim gitconfig gitignore_global"  # list of files/folders to symlink in homedir
+files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc vim gitconfig gitignore_global screenrc"  # list of files/folders to symlink in homedir
 
 ##########
 
