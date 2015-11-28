79ce2930e48aa96c37b6be3b55edf716bfab6061
Tatum <tatum@ashlandstudios.com>
2015-11-27 19:17:46 -0600

    add npmrc to the setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc.after gitconfig gitignore_global screenrc"  # list of files/folders to symlink in homedir
+files="bashrc bash_alias bash_profile irbrc inputrc gemrc vimrc.after gitconfig gitignore_global screenrc npmrc"  # list of files/folders to symlink in homedir
 
 ##########
 
