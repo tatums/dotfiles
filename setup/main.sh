279203a6f6684a98868092bdabde528f486395c0
Tatum Szymczak <tatum@ashlandstudios.com>
2016-03-24 22:08:39 -0500

    fix path

--- setup/main.sh
+++ setup/main.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ## 2 homebrew
 ## 3 vim-setup.rb
 
-sh ${DIR}/lib/dotfiles.sh
-sh ${DIR}/lib/homebrew.sh
-ruby ${DIR}/lib/vim-setup.rb
+sh ${DIR}/setup/lib/dotfiles.sh
+sh ${DIR}/setup/lib/homebrew.sh
+ruby ${DIR}/setup/lib/vim-setup.rb
